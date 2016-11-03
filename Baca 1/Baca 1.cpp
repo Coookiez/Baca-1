185162b2451e88fd2cfde5eb834412c335c6562e
Coookiez <insanecookiez22@gmail.com>
2016-11-03 23:52:33 +0100

    Fixed many bugs

--- Baca 1/Baca 1.cpp
+++ Baca 1/Baca 1.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 struct slistEl {
 	slistEl *next;
-	int roomNumber, distance, predecessor, priceOfEntrance;
+	int roomNumber, distance, predecessor, priceOfEntrance, numberOfNeighbours;
 	bool QS;
 
 	slistEl() {
@@ -16,6 +16,7 @@ struct slistEl {
 		this->distance = INT32_MAX;
 		this->predecessor = -1;
 		this->priceOfEntrance = 0;
+		this->numberOfNeighbours = 2;
 		this->QS = false;
 	}// numer wêz³a docelowego i waga krawêdzi
 };
@@ -50,81 +51,77 @@ int main() {
 		cin >> numberOfBargains;
 		for (int j = 0; j < m; ++j) {
 			for (int k = 0; k < n; ++k) {
-				slistEl *pokoj1 = new slistEl();
-				slistEl *pokoj2 = new slistEl();
-				graf[j * n + k].next = pokoj1;
+				slistEl *pokoj1;
+				slistEl *pokoj2;
 				if (j * n + k == 0) { // Left down
-					*pokoj1 = graf[j * n + 1];
-					*pokoj2 = graf[(j + 1) * n];
+					pokoj1 = &graf[j * n + 1];
+					//	pokoj1->roomNumber;
+					pokoj2 = &graf[(j + 1) * n];
 				} else if (j * n + k == n - 1) { // Right down
-					*pokoj1 = graf[j * n + k - 1];
-					*pokoj2 = graf[(j + 1) * n + k];
+					pokoj1 = &graf[j * n + k - 1];
+					pokoj2 = &graf[(j + 1) * n + k];
 				} else if (j * n + k == n * (m - 1)) { // Left up
-					*pokoj1 = graf[(j - 1) * n + k];
-					*pokoj2 = graf[j * n + k + 1];
+					pokoj1 = &graf[(j - 1) * n + k];
+					pokoj2 = &graf[j * n + k + 1];
 				} else if (j * n + k == m * n - 1) { // Right Up
-					*pokoj1 = graf[j * n + k - 1];
-					*pokoj2 = graf[(j - 1) * n + k];
+					pokoj1 = &graf[j * n + k - 1];
+					pokoj2 = &graf[(j - 1) * n + k];
 				} else {
-					slistEl *pokoj3 = new slistEl();
+					slistEl *pokoj3;
+					graf[j*n + k].numberOfNeighbours = 3;
 					if ((j == 0 || j == m - 1) && (k > 0 && k < n - 1)) { // Top / Bottom edge
-						pokoj1->roomNumber = j * n + k - 1;
-						pokoj2->roomNumber = j * n + k + 1;
+						pokoj1 = &graf[j * n + k - 1];
+						pokoj2 = &graf[j * n + k + 1];
 						if (j == 0) { // Bottom edge
-							pokoj3->roomNumber = (j + 1) * n + k;
+							pokoj3 = &graf[(j + 1)*n + k];
 						} else { // Top edge
-							pokoj3->roomNumber = (j - 1) * n + k;
+							pokoj3 = &graf[(j - 1) * n + k];
 						}
 					} else if ((k == 0 || k == n - 1) && (j > 0 && j < m - 1)) { // Left / Right edge
-						pokoj1->roomNumber = (j + 1) * n + k;
-						pokoj2->roomNumber = (j - 1) * n + k;
+						pokoj1 = &graf[(j + 1) * n + k];
+						pokoj2 = &graf[(j - 1) * n + k];
 						if (k == 0) { // Left Edge
-							pokoj3->roomNumber = j * n + k + 1;
+							pokoj3 = &graf[j * n + k + 1];
 						} else { // Right Edge
-							pokoj3->roomNumber = j * n + k - 1;
+							pokoj3 = &graf[j * n + k - 1];
 						}
 					} else {
-						slistEl *pokoj4 = new slistEl();
-						pokoj1->roomNumber = j * n + k - 1;
-						pokoj2->roomNumber = j * n + k + 1;
-						pokoj3->roomNumber = (j - 1) * n + k;
-						pokoj4->roomNumber = (j + 1) * n + k;
+						slistEl *pokoj4;
+						graf[j*n + k].numberOfNeighbours = 4;
+						pokoj1 = &graf[j * n + k - 1];
+						pokoj2 = &graf[j * n + k + 1];
+						pokoj3 = &graf[(j - 1) * n + k];
+						pokoj4 = &graf[(j + 1) * n + k];
 						pokoj3->next = pokoj4;
 					}
 					pokoj2->next = pokoj3;
 				}
 				pokoj1->next = pokoj2;
+				graf[j * n + k].next = pokoj1;
 			}
 		}
-		wypisywanie(m, n, graf);
-		/*cout << " przed wpisywaniem promocyjnych pokojow" << endl;
 		for (int j = 0; j < 2 * numberOfBargains; j += 2) {
 			cin >> roomsToVisit[j] >> roomsToVisit[j + 1];
-			graf[(n - roomsToVisit[j]) * m + roomsToVisit[j + 1]].priceOfEntrance *= -1;
+			cout << roomsToVisit[j] << " " << roomsToVisit[j + 1] << " " << (m - roomsToVisit[j])*n + roomsToVisit[j + 1] - 1 << endl;
+			graf[(m - roomsToVisit[j]) * n + roomsToVisit[j + 1] - 1].priceOfEntrance *= -1;
 		}
-		cout << " po wpisaniu promocyjnych cen" << endl;
+
 		for (int j = 0; j < m * n; j++) {
 			cout << " FIRST LOOP:  j: " << j << endl;
-
-			for (k = 0; graf[k].QS; k++)
-				cout << " SECOND LOOP:  k: " << k << endl;
-			for (l = k++; l < m * n; k++) {
-				if (!graf[k].QS && graf[k].distance < graf[l].distance) {
-					l = k;
-				}
-			}
-			graf[l].QS = true;
-
-			for (*pw = graf[l]; pw; pw->next) {
+			for (k = j; graf[k].QS; k++);
+			cout << " SECOND LOOP:  k: " << k << endl;
+			graf[k].QS = true;
+			pw = &graf[k];
+			for (int z = 0; z < graf[k].numberOfNeighbours; z++, pw = pw->next) {
+				cout << "iteracja, graf[k].numberOfN: " << graf[k].numberOfNeighbours << endl;
 				if (!graf[pw->roomNumber].QS &&
-					(graf[pw->roomNumber].distance > graf[l].distance + graf[pw->roomNumber].priceOfEntrance)) {
-					graf[pw->roomNumber].distance = graf[l].distance + graf[pw->roomNumber].priceOfEntrance;
+					(graf[pw->roomNumber].distance > graf[k].distance + graf[pw->roomNumber].priceOfEntrance)) {
+					graf[pw->roomNumber].distance = graf[k].distance + graf[pw->roomNumber].priceOfEntrance;
 				}
 			}
 		}
-		for (int j = 0; j < numberOfBargains; j += 2) {
-			cout << graf[(n - roomsToVisit[j]) * m + roomsToVisit[j + 1]].distance;
-		}*/
+		cout << graf[8].priceOfEntrance;
+		delete[] graf;
 	}
 	return 0;
 }
